f07e79960ee5c42b463d8fe275aea6cef5049646
hector_oliveira <hector@b42.com.br>
2022-03-30 10:01:14 -0300

    primeiro commit

---
+++ database/migrations/tenant/2022_03_25_194920_create_aluno_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlunoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aluno', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 50)->unique();
+            $table->integer('idade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aluno');
+    }
+}
